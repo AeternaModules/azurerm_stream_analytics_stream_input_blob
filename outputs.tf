3a57cf9682fb4114e2039d5bb88b41aa873187ec
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:00 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,46 @@
-output "stream_analytics_stream_input_blobs" {
-  description = "All stream_analytics_stream_input_blob resources"
-  value       = azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs
-  sensitive   = true
-}
 output "stream_analytics_stream_input_blobs_authentication_mode" {
-  description = "List of authentication_mode values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.authentication_mode]
+  description = "Map of authentication_mode values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.authentication_mode }
 }
 output "stream_analytics_stream_input_blobs_date_format" {
-  description = "List of date_format values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.date_format]
+  description = "Map of date_format values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.date_format }
 }
 output "stream_analytics_stream_input_blobs_name" {
-  description = "List of name values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.name]
+  description = "Map of name values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.name }
 }
 output "stream_analytics_stream_input_blobs_path_pattern" {
-  description = "List of path_pattern values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.path_pattern]
+  description = "Map of path_pattern values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.path_pattern }
 }
 output "stream_analytics_stream_input_blobs_resource_group_name" {
-  description = "List of resource_group_name values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.resource_group_name]
+  description = "Map of resource_group_name values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.resource_group_name }
 }
 output "stream_analytics_stream_input_blobs_serialization" {
-  description = "List of serialization values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.serialization]
+  description = "Map of serialization values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.serialization }
 }
 output "stream_analytics_stream_input_blobs_storage_account_key" {
-  description = "List of storage_account_key values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.storage_account_key]
+  description = "Map of storage_account_key values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.storage_account_key }
   sensitive   = true
 }
 output "stream_analytics_stream_input_blobs_storage_account_name" {
-  description = "List of storage_account_name values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.storage_account_name]
+  description = "Map of storage_account_name values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.storage_account_name }
 }
 output "stream_analytics_stream_input_blobs_storage_container_name" {
-  description = "List of storage_container_name values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.storage_container_name]
+  description = "Map of storage_container_name values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.storage_container_name }
 }
 output "stream_analytics_stream_input_blobs_stream_analytics_job_name" {
-  description = "List of stream_analytics_job_name values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.stream_analytics_job_name]
+  description = "Map of stream_analytics_job_name values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.stream_analytics_job_name }
 }
 output "stream_analytics_stream_input_blobs_time_format" {
-  description = "List of time_format values across all stream_analytics_stream_input_blobs"
-  value       = [for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : v.time_format]
+  description = "Map of time_format values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.time_format }
 }
 
