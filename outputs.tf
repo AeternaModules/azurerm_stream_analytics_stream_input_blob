6bb9a3ee0c7dffc215bfa58ade14ce94421adf50
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:04:46 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -24,7 +24,7 @@ output "stream_analytics_stream_input_blobs_resource_group_name" {
 }
 output "stream_analytics_stream_input_blobs_serialization" {
   description = "Map of serialization values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
-  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => v.serialization if v.serialization != null && length(v.serialization) > 0 }
+  value       = { for k, v in azurerm_stream_analytics_stream_input_blob.stream_analytics_stream_input_blobs : k => one(v.serialization) if v.serialization != null && length(v.serialization) > 0 }
 }
 output "stream_analytics_stream_input_blobs_storage_account_key" {
   description = "Map of storage_account_key values across all stream_analytics_stream_input_blobs, keyed the same as var.stream_analytics_stream_input_blobs"
